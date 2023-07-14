e8eb79a7c1070b949dd0825832a1843f918162d7
HChandeepa <118461284+HChandeepa@users.noreply.github.com>
2023-07-14 09:45:28 +0530

    data

--- data.php
+++ data.php
@@ -1,5 +1,22 @@
+<html>
 
-<meta charset="utf-8">
+<head>
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="web.css" rel="stylesheet" type="text/css">
+
     <title>Ella</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <form action="" method="post" class="form" placeholder="N">
+            <input type="text" class="name" name="name">
+            <textarea name="message" cols="30" rows="10" class="message" placeholder="N"></textarea>
+
+        </form>
+    </div>
+</body>
+
+<html>
