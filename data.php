0e3502f11690159a3e29c4d235b0e9b95d6855f4
HChandeepa <118461284+HChandeepa@users.noreply.github.com>
2023-07-14 10:20:50 +0530

    config

--- data.php
+++ data.php
@@ -1,67 +1,74 @@
 <?php
 
-	include 'config.php';
+include 'config.php';
 
-	if (isset($_POST['post_comment'])) {
+if (isset($_POST['post_comment'])) {
 
-		$name = $_POST['name'];
-		$message = $_POST['message'];
+    $name = $_POST['name'];
+    $message = $_POST['message'];
 
-		$sql = "INSERT INTO demo (name, message)
+    $sql = "INSERT INTO demo (name, message)
 		VALUES ('$name', '$message')";
 
-		if ($conn->query($sql) === TRUE) {
-		  echo "";
-		} else {
-		  echo "Error: " . $sql . "<br>" . $conn->error;
-		}
-	}
+    if ($conn->query($sql) === TRUE) {
+        echo "";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<<<<<<< HEAD
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="style.css">
-	<title>Comment System PHP | National Coding</title>
-=======
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link href="https://github.com/HChandeepa/Ella/blob/main/web.css" rel="stylesheet" type="text/css">
+    <<<<<<< HEAD <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="web.css">
+        <title>Comment System PHP | National Coding</title>
+        =======
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link href="https://github.com/HChandeepa/Ella/blob/main/web.css" rel="stylesheet" type="text/css">
 
-    <title>Ella</title>
->>>>>>> 8d90f5d7ea9bc59dda74fa15e1cea0c3afff5abf
+        <title>Ella</title>
+        >>>>>>> 8d90f5d7ea9bc59dda74fa15e1cea0c3afff5abf
 </head>
+
 <body>
-	<div class="wrapper">
-		<form action="" method="post" class="form">
-			<input type="text" class="name" name="name" placeholder="Name">
-			<br>
-			<textarea name="message" cols="30" rows="10" class="message" placeholder="Message"></textarea>
-			<br>
-			<button type="submit" class="btn" name="post_comment">Post Comment</button>
-		</form>
-	</div>
+    <div class="wrapper">
+        <form action="" method="post" class="form">
+            <input type="text" class="name" name="name" placeholder="Name">
+            <br>
+            <textarea name="message" cols="30" rows="10" class="message" placeholder="Message"></textarea>
+            <br>
+            <button type="submit" class="btn" name="post_comment">Post Comment</button>
+        </form>
+    </div>
 
-	<div class="content">
-		<?php
+    <div class="content">
+        <?php
 
-			$sql = "SELECT * FROM demo";
-			$result = $conn->query($sql);
+        $sql = "SELECT * FROM demo";
+        $result = $conn->query($sql);
 
-			if ($result->num_rows > 0) {
-			  // output data of each row
-			  while($row = $result->fetch_assoc()) {
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while ($row = $result->fetch_assoc()) {
 
-		?>
-		<h3><?php echo $row['name']; ?></h3>
-		<p><?php echo $row['message']; ?></p>
+                ?>
+                <h3>
+                    <?php echo $row['name']; ?>
+                </h3>
+                <p>
+                    <?php echo $row['message']; ?>
+                </p>
 
-		<?php } } ?>
-	</div>
+            <?php }
+        } ?>
+    </div>
 </body>
+
 </html>
