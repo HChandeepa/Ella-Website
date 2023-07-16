c319136ccbb9b2329475959e08fbce1faa9e248b
HChandeepa <118461284+HChandeepa@users.noreply.github.com>
2023-07-16 21:36:19 +0530

    popup message

--- j.js
+++ j.js
@@ -1,2 +1,19 @@
 const date = new Date();
 document.getElementById("date").innerHTML =date;
+
+// Function to show the popup
+function showPopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+}
+
+// Function to close the popup
+function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
+
+// Show the popup when the page loads
+window.onload = function () {
+    showPopup();
+};
