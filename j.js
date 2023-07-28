d904da2e81205bf8a29ff9263ed111707cbbe876
HChandeepa <118461284+HChandeepa@users.noreply.github.com>
2023-07-28 12:52:39 +0530

    popup message

--- j.js
+++ j.js
@@ -17,6 +17,25 @@ function closePopup() {
 window.onload = function () {
     showPopup();
 };
+/*video*/
+// Function to show the popup
+function showPopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "block";
+}
+
+// Function to close the popup
+function closePopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "none";
+}
+
+// Show the popup when the page loads
+window.onload = function () {
+  showPopup();
+};
+
+/*video end*/
 
 let slideIndex = 0;
 showSlides();
